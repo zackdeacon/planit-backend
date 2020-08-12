90d369b117ed01dfbb151795696c4f252d68f5a0
Vinny V <VinAVarghese@gmail.com>
2020-08-12 15:22:34 -0700

    nav bar initialized. Cleaned up file structure on front end.

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from "../../components/NavBar/navbar"
+
+export default function Home() {
+    return (
+        <>
+           <Navbar /> 
+        </>
+    )
+}
