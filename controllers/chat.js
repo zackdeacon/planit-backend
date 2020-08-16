0c6861fbb861ec191298d7bec5492ed4aed294a7
nicoleremy95 <nicoleremy95@gmail.com>
2020-08-16 16:09:40 -0700

    fixed trailing syntax errors

--- controllers/chat.js
+++ controllers/chat.js
@@ -29,38 +29,39 @@ router.get("/map/:mapId", (req, res) => {
   })
 })
 
-// Add a new chat
-// Passed test call
-  db.Chat.create({
-    userId: req.body.userId,
-    mapId: req.body.mapId,
-    message: req.body.message
-  }).then(newChat => {
-    res.json(newChat)
-    res.status(204).end()
-  }).catch(err => {
-    console.log(err)
-    res.status(500).end()
-  })
-})
+// // Add a new chat
+// // Passed test call
+//   db.Chat.create({
+//     userId: req.body.userId,
+//     mapId: req.body.mapId,
+//     message: req.body.message
+//   }).then(newChat => {
+//     res.json(newChat)
+//     res.status(204).end()
+//   }).catch(err => {
+//     console.log(err)
+//     res.status(500).end()
+//   })
+// })
 
 // Session Conditional >> When ready, uncomment
-// router.post("/new", (req, res) => {
-//   if(!req.session.user){
-//     res.status(401).send("login required")
-//   } else{
-//     db.Chat.create({
-//       userId: req.session.user.id,
-//       mapId: req.body.mapId,
-//       message: req.body.message
-//     }).then(newChat => {
-//       res.json(newChat)
-//       res.status(204).end()
-//     }).catch(err => {
-//       console.log(err)
-//       res.status(500).end()
-//     })
-//   }
+router.post("/new", (req, res) => {
+  if(!req.session.user){
+    res.status(401).send("login required")
+  } else{
+    db.Chat.create({
+      userId: req.session.user.id,
+      mapId: req.body.mapId,
+      message: req.body.message
+    }).then(newChat => {
+      res.json(newChat)
+      res.status(204).end()
+    }).catch(err => {
+      console.log(err)
+      res.status(500).end()
+    })
+  }
+})
   
 // Delete chat
 // Passed test call
--- controllers/suggestion.js
+++ controllers/suggestion.js
@@ -36,7 +36,8 @@ router.post("/new", (req, res) => {
       console.log(err);
       res.status(500).end();
     });
-  // }
+  })
+
 
 // Delete a suggestion by id
 // Passed a test call
