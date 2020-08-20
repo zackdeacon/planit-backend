3c1fea97202e6b121490a5eb385aea04803cb8cb
Vinny V <VinAVarghese@gmail.com>
2020-08-20 11:19:19 -0700

    heroku set up

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to MongoDB
 // Change boolean to true to reseed database on server start
-const reseedOnConnect = true;
+const reseedOnConnect = false;
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/plannit", {
     useNewUrlParser: true,
@@ -46,9 +46,7 @@ mongoose
 // Uncomment for development
 app.use(
   cors({
-    origin: ["http://localhost:3000", 
-    "http://localhost:3000/chat"
-  ],
+    origin: ["https://travelplanit.herokuapp.com", "http://localhost:3000"],
     credentials: true,
   })
 );
@@ -60,11 +58,11 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    // proxy: true,
+    proxy: true,
     cookie: {
       maxAge: 2 * 60 * 60 * 1000,
-      // sameSite: "none",
-      // secure: true,
+      sameSite: "none",
+      secure: true,
     },
   })
 );
