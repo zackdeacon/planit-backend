743d82b47c96423a256a1d100cbca2c48cb2d9e8
Derek Watson <derek.watson92@gmail.com>
2020-08-21 14:55:36 -0700

    added routes/nodemailer stuff for final render email

--- controllers/index.js
+++ controllers/index.js
@@ -6,11 +6,13 @@ const mapRoutes = require("./map");
 const suggestionRoutes = require("./suggestion");
 const chatRoutes = require("./chat");
 const authRoutes = require("./auth");
+const emailRoutes = require("./email");
 
 router.use("/api/users", userRoutes);
 router.use("/api/maps", mapRoutes);
 router.use("/api/suggestions", suggestionRoutes);
 router.use("/api/chats", chatRoutes);
 router.use("/api/auth", authRoutes);
+router.use("/api/email", emailRoutes);
 
 module.exports = router;
