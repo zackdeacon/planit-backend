72ee4020a95fd1cb2769f438e6deed5aa84a1797
Vinny V <VinAVarghese@gmail.com>
2020-08-28 10:38:39 -0700

    reverting to original

--- server.js
+++ server.js
@@ -60,24 +60,23 @@ mongoose
 // })
 
 // Vin's CORs Alternative
-app.use(function(req, res, next) {
-  res.header('credentials', 'include');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', "https://travelplanit.herokuapp.com");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('credentials', 'include');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header('Access-Control-Allow-Origin', "https://travelplanit.herokuapp.com");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+//   next();
+// });
 
 // CORS
-// Uncomment for development
-// app.use(
-//   cors({
-//     // origin: "http://localhost:3000",
-//     origin: "https://travelplanit.herokuapp.com",
-//     credentials: true
-//   })
-// );
+app.use(
+  cors({
+    // origin: "http://localhost:3000",
+    origin: "https://travelplanit.herokuapp.com",
+    credentials: true
+  })
+);
 
 //SESSION
 // for heroku deploy uncomment proxy, samesite and secure
