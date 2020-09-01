ef06fa5c3740da799328276f6f380300ad4de32e
Vinny V <VinAVarghese@gmail.com>
2020-09-01 11:22:16 -0700

    resetting CORs

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ mongoose
 //   gfs.collection("Map")
 // })
 
-// Vin's CORs Alternatives
+// VIN'S CORs ALTERNATIVES
 // app.use(function(req, res, next) {
 //   res.header('credentials', 'include');
 //   res.header('Access-Control-Allow-Credentials', true);
@@ -68,32 +68,29 @@ mongoose
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 //   next();
 // });
+//
+// const whitelist = ["https://travelplanit.herokuapp.com", "http://localhost:3000"]
+// var corsOptionsDelegate = (req, callback) => {
+//   var corsOption;
 
-const whitelist = ["https://travelplanit.herokuapp.com", "http://localhost:3000"]
-var corsOptionsDelegate = (req, callback) => {
-  var corsOption;
-
-  if(whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = {origin:true};
-  } else {
-    corsOptions = {origin: false};
-  }
-  callback(null, corsOptions);
-};
+//   if(whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = {origin:true};
+//   } else {
+//     corsOptions = {origin: false};
+//   }
+//   callback(null, corsOptions);
+// };
+// app.use(
+//   cors(corsOptionsDelegate)
+// );
 
 // CORS
 app.use(
-  cors(corsOptionsDelegate)
+  cors({
+    origin: ["https://travelplanit.herokuapp.com","http://localhost:3000"],
+    credentials: true
+  })
 );
-
-// CORS
-// app.use(
-//   cors({
-//     // origin: "http://localhost:3000",
-//     origin: "https://travelplanit.herokuapp.com",
-//     credentials: true
-//   })
-// );
 
 //SESSION
 // for heroku deploy uncomment proxy, samesite and secure
